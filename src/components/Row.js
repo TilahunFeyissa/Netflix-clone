c7230ae1c01a99a1bc334bb2b036ea14fa9524ed
Tilahun <totilahunfeyissa@gmail.com>
2022-07-16 18:25:35 +0300

    first commit

---
+++ src/components/Row.js
@@ -0,0 +1,91 @@
+import React, { useEffect, useState } from 'react'
+import './Row.css'
+import axios from './axios'
+import requests from './requests';
+import YouTube from 'react-youtube';
+import movieTrailer from 'movie-trailer'
+
+
+
+
+const base_url = "https://image.tmdb.org/t/p/original/";
+
+
+
+function Row({title,fetchUrl,isLargeRow}) {
+  console.log(requests)
+
+    const [movies, setmovies]=useState([])
+    const[trailerUrl,setTrailerUrl]=useState("")
+    useEffect(()=>{
+        async function fetchData(){
+            const Requests = await axios.get(fetchUrl);
+            // console.log(Requests)
+            setmovies(Requests.data.results)
+        }
+        fetchData()},[fetchUrl])
+        console.log(movies)
+
+
+
+
+
+
+
+
+
+        const opts = {
+            heighr: "390",
+            width: "100%",
+            playerVars: {
+              autoplay: 1,
+            }
+          }
+
+        const handleClick = (movie) => {
+            if(trailerUrl){
+              setTrailerUrl('')
+            }else{
+                movieTrailer(movie?.title 
+                    || movie?.name || movie.original_name
+                    )
+                    .then((url) => {
+                        const urlParams = new URLSearchParams(new URL(url).search)
+                        setTrailerUrl(urlParams.get('v'))
+                      })
+                .catch((error)=>console.log(error));
+            }
+        };
+    
+  return (
+    <div className='row'>
+    <h1>{title}</h1>
+    <div className='row__posters'>
+        {movies.map((movie)=>(
+            <img 
+            key={movie.id}
+            onClick={()=>handleClick(movie)} 
+            className={`row__poster ${isLargeRow && "row__posterLarge"}`} 
+            src={`${base_url}${isLargeRow?movie.poster_path:movie.backdrop_path}`}
+            alt={movie.name}/>
+
+
+
+
+
+
+        ))} 
+
+
+    </div>
+    <div style={{ padding: "40px"}}>
+       {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
+      </div>
+    </div>
+  )
+}
+
+export default Row
+
+
+
---
+++ src/components/requests.js
@@ -0,0 +1,24 @@
+const API_KEY="e84bb06d913864c43b6f81964a08e01b"
+
+const requests = {
+    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+    
+    fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+    fetchTopRatedMovies: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+    fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+    fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+    fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+    fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+    fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+    
+  };
+  export default requests;
+
+
+//   // https://api.themoviedb.org/3/discover/tv?api_key=e84bb06d913864c43b6f81964a08e01b&with_networks=213
+
+
+
+  
+
+//https://api.themoviedb.org/3/trending/all/week?
